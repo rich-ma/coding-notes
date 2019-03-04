a40807f4164d1a21ef68d4541fe82421d8e66600
Richard Ma <rich.maaa@gmail.com>
2019-03-04 01:58:35 -0800

    Finish code

--- leetCode/longestSubStrings.js
+++ leetCode/longestSubStrings.js
@@ -27,3 +27,26 @@
 // Explanation: The answer is "wke", with the length of 3.
 // Note that the answer must be a substring, "pwke"
 // is a subsequence and not a substring.
+
+var lengthOfLongestSubstring = function (s) {
+  if (s.length === 0) return 0;
+  longestWord = s[0];
+  currWord = s[0];
+  let i = 0,
+    letter;
+  for (let j = 1; j < s.length; j++) {
+    letter = s[j];
+    if (currWord.includes(letter)) {
+      i = i + s.slice(i).indexOf(letter) + 1;
+      currWord = s.slice(i, j);
+    }
+    currWord = currWord + letter;
+    if (currWord.length > longestWord.length) longestWord = currWord;
+
+  }
+  return longestWord.length;
+};
+// Runtime: 92 ms, faster than 88.27 % of JavaScript online submissions
+// for Longest Substring Without Repeating Characters.
+// Memory Usage: 41.2 MB, less than 46.39 % of JavaScript online submissions
+// for Longest Substring Without Repeating Characters.

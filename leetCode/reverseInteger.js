be2b06c05424288903765042ad8214eb06ab8a6d
Richard Ma <rich.maaa@gmail.com>
2019-03-04 15:49:01 -0800

    Finish code

--- leetCode/reverseInteger.js
+++ leetCode/reverseInteger.js
@@ -27,3 +27,18 @@ Note:
   Assume we are dealing with an environment which could only store integers within the 32 - bit signed integer range: [−231, 231− 1].For the purpose of this problem, assume that your
 function returns 0 when the reversed integer overflows.
 */
+var reverse = function (x) {
+  let modifier = x < 0 ? true : false;
+  x = x.toString().split('').reverse().join('');
+  if (modifier) {
+    x = '-' + x.slice(0, x.length - 1);
+  }
+  return Math.abs(parseInt(x)) > 2147483647 ? 0 : parseInt(x);
+};
+
+/*
+Runtime: 84 ms, faster than 58.84 % of JavaScript online submissions
+for Reverse Integer.
+Memory Usage: 35.8 MB, less than 70.03 % of JavaScript online submissions
+for Reverse Integer.
+*/

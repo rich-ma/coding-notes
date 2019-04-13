342be8b2c5f6a69010a3f2ef06f49b9e034878d3
Richard Ma <rich.maaa@gmail.com>
2019-04-12 19:07:14 -0700

    Removed global result var

--- AA study/memoization_project/lib/leet_code_518.js
+++ AA study/memoization_project/lib/leet_code_518.js
@@ -2,29 +2,26 @@
 // Feel free to use this file for scratch work.
 
 function change(amount, coins) {
-  if (amount === 0) return [];
-  result = [];
+  if (amount === 0) return [[]];
+  let result = [];
 
   coins.forEach(coin => {
     if (amount >= coin) {
       let curr_change = change(amount - coin, coins);
-
-      if(curr_change){
-        curr_change.forEach( combination => {
+      if (curr_change) {
+        curr_change.forEach(combination => {
           combination.push(coin);
           combination.sort();
-
-          
-          
+          if(result.every(prev_res => {
+              !_.isEqual(prev_res, combination);
+            })){
+            result.push(combination);
+          }
         });
-
-        
       };
-
     }
-
   });
 
-  if (result.length === 0) return false;
+  if (result.length === 0) return 0;
   return result;
 };

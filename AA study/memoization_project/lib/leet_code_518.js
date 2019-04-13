b74a7550eb48c730ed97aa16e50ff4ad80aeb929
Richard Ma <rich.maaa@gmail.com>
2019-04-12 18:02:57 -0700

    Test leetcode change question

--- AA study/memoization_project/lib/leet_code_518.js
+++ AA study/memoization_project/lib/leet_code_518.js
@@ -1,3 +1,30 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+function change(amount, coins) {
+  if (amount === 0) return [];
+  result = [];
+
+  coins.forEach(coin => {
+    if (amount >= coin) {
+      let curr_change = change(amount - coin, coins);
+
+      if(curr_change){
+        curr_change.forEach( combination => {
+          combination.push(coin);
+          combination.sort();
+
+          
+          
+        });
+
+        
+      };
+
+    }
+
+  });
+
+  if (result.length === 0) return false;
+  return result;
+};

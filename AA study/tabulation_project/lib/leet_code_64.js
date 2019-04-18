2e18fcb82236d6c42847e009f5934376a8af7682
Richard Ma <rich.maaa@gmail.com>
2019-04-17 22:48:48 -0700

    Finish one version of minPathSum

--- AA study/tabulation_project/lib/leet_code_64.js
+++ AA study/tabulation_project/lib/leet_code_64.js
@@ -2,5 +2,23 @@
 // Feel free to use this file for scratch work.
 
 function minPathSum(grid) {
+	let table = new Array(grid.length).fill().map(()=> new Array(grid[0].length).fill(Infinity));
+	table[0][0] = grid[0][0];
 
+	for( let m = 0; m < grid.length; m++){
+		for( let n = 0; n < grid[0].length; n++){
+			if(m === 0 && n==0) continue;
+			let top = m-1 >= 0 ? table[m - 1][n] : Infinity;
+			let left = n - 1 >= 0 ? table[m][n - 1] : Infinity;
+			table[m][n] = top < left ? top + grid[m][n] : left + grid[m][n];
+		}
+	}
+
+	return table[grid.length-1][grid[0].length-1];
 }
+
+console.log(minPathSum([
+	[1, 3, 1],
+	[1, 5, 1],
+	[4, 2, 1]
+]));

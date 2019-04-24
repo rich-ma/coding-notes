4395353892a9c49e0157d319e3c90266c3c987e3
Richard Ma <rich.maaa@gmail.com>
2019-04-23 20:53:39 -0700

    Finish merge sort

--- AA study/merge_sort_project/lib/merge_sort.js
+++ AA study/merge_sort_project/lib/merge_sort.js
@@ -1,9 +1,20 @@
 function merge(array1, array2) {
-
+	let result = [];
+	while(array1.length && array2.length){
+		let lowest = (array1[0] < array2[0] ? array1.shift() : array2.shift());
+		result.push(lowest);
+	}
+	return result.concat(array1.concat(array2));
 }
 
 function mergeSort(array) {
+	if(array.length <= 1) return array;
 
+	let mid = Math.floor(array.length/2);
+	let left = mergeSort(array.slice(0,mid));
+	let right = mergeSort(array.slice(mid));
+
+	return merge(left, right);
 }
 
 module.exports = {

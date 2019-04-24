d07fac0c0db773fd9a93856fa5f64d2df68f5ab8
Richard Ma <rich.maaa@gmail.com>
2019-04-23 23:05:02 -0700

    Finished counter using a POJO instead of array

--- AA study/counting_sort_project/lib/counting_sort.js
+++ AA study/counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,33 @@
 function countingSort(arr, max) {
+	let counter = new Array(max + 1).fill(0);
+	arr.forEach( num => counter[num]++);
+	
+	result = [];
+	counter.forEach((times, num) => {
+		for(let i = 0; i < times; i++){
+			result.push(num);
+		}
+	})
 
+	return result;
+
+	// let counter = {};
+	// arr.forEach( num => {
+	// 	if(!counter[num]) {
+	// 		counter[num] = 1;
+	// 	} else {
+	// 		counter[num]++;
+	// 	}
+	// });
+
+	// let result = [];
+	// for(num in counter){
+	// 	console.log(num, counter[num]);
+	// 	for(let i = 0; i < counter[num]; i++){
+	// 		result.push(parseInt(num));
+	// 	}
+	// }
+	// return result;
 }
 
 

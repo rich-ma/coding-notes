9b78d5adc037994744f9e408c473c76d1c87f705
Richard Ma <rich.maaa@gmail.com>
2019-09-16 23:28:15 -0700

    Add comments and comment form

--- reduxPractice/learn-redux/client/reducers/comments.js
+++ reduxPractice/learn-redux/client/reducers/comments.js
@@ -1,6 +1,21 @@
 function comments(state = [], action) {
-	console.log(state, action);
-	return state;
+
+	console.log('action', action.postId);
+	let newState = Object.assign({}, state);
+	console.log('newState', newState);
+
+	switch(action.type){
+		case 'ADD_COMMENT':
+			let postId = action.postId;
+			newState[postId].push({
+				text: action.comment, 
+				user: action.author
+			})
+			console.log(newState[postId]);
+			return newState;
+		default:
+			return state;
+	}
 }
 
 export default comments;

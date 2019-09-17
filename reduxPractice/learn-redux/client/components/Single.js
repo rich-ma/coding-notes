8c3c045e557491910a83cd8ba5b1f3036ca3f9eb
Richard Ma <rich.maaa@gmail.com>
2019-09-16 18:59:14 -0700

    Finish part 13,14

--- reduxPractice/learn-redux/client/components/Single.js
+++ reduxPractice/learn-redux/client/components/Single.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
+  render() {
+    const { postId } = this.props.params;
 
-	render(){
-		return(
-			<div className='single'>
-				
-			</div>
-		)
-	}
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    const post = this.props.posts[i];
+
+    const postComments = this.props.comments[postId] || [];
+
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} />
+      </div>
+    )
+  }
 });
 
 export default Single;
+

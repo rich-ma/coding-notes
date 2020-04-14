325a843b1a044333b7e7378c4106d56e669c2ca6
Richard Ma <rich.maaa@gmail.com>
2020-04-13 22:08:23 -0700

    finish 04

--- AA study/review/advanced_methods_exercise/04_filter_lengths.rb
+++ AA study/review/advanced_methods_exercise/04_filter_lengths.rb
@@ -4,6 +4,11 @@
 # is passed in, then 5 should be used as the length.
 
 
+def filter_lengths(strings, length = 5)
+	strings.select {|str| str.length >= length}
+end
+
+
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"])      # => ["dragonfruit", "clementine"]
 p filter_lengths(["cat", "dog", "capybara", "mouse"], 7)            # => ["capybara"]

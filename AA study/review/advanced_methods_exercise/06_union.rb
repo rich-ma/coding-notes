69677ed6592392c43d010165222bb453df5e18da
Richard Ma <rich.maaa@gmail.com>
2020-04-13 22:30:15 -0700

    finish 06

--- AA study/review/advanced_methods_exercise/06_union.rb
+++ AA study/review/advanced_methods_exercise/06_union.rb
@@ -1,6 +1,10 @@
 # Write a method, union, that accepts any number of arrays as arguments.
 # The method should return an array containing all elements of the given arrays.
-
+def union(*arrays)
+	arrays.inject {|result, arr| result.concat(arr)}
+end
 
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
 p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
+
+# use inject to carry a result array where we concat all the arg arrays passed in

4d3481fe8194b0b8c8dc28f5ce5efa937ba5a07c
Richard Ma <rich.maaa@gmail.com>
2020-04-13 22:16:37 -0700

    finish 05

--- AA study/review/advanced_methods_exercise/05_max_inject.rb
+++ AA study/review/advanced_methods_exercise/05_max_inject.rb
@@ -1,6 +1,11 @@
 # Write a method, max_inject(arr), that accepts any amount of numbers arguments and returns
 # the largest number. Solve this using the built-in inject.
-
+def max_inject(*arr)
+	arr.inject {|max, el| el > max ? el : max}
+end
 
 p max_inject(1, -4, 0, 7, 5)  # => 7
 p max_inject(30, 28, 18)      # => 30
+
+# use inject to bubble the largest value in the arr/arguments up to the end as a return
+# we use splat to accept a large number of values like an array

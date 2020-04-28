de2b27f98648d94d9f44bbcd3ac2d71bca24dea8
Richard Ma <rich.maaa@gmail.com>
2020-04-27 18:33:34 -0700

    finish 07 and adv methods

--- AA study/review/advanced_methods_exercise/07_multi_dimensional_sum.rb
+++ AA study/review/advanced_methods_exercise/07_multi_dimensional_sum.rb
@@ -1,6 +1,15 @@
 # Write a method, multi_dimensional_sum(array), that accepts a multidimensional array as an arg 
 # and returns the sum of all numbers in the array.
 
+def multi_dimensional_sum(arr)
+	arr.inject(0) do |acc, el|
+		if el.is_a?(Array)
+			multi_dimensional_sum(el) + acc
+		else
+			acc + el
+		end
+	end
+end
 
 arr_1 = [
     [4, 3, 1],

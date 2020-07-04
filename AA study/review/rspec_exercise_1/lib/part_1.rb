a0b3b7401520edab9ad5d348fe29b9754ad0663d
Richard Ma <rich.maaa@gmail.com>
2020-07-03 19:17:41 -0700

    finish rspec prac 1

--- AA study/review/rspec_exercise_1/lib/part_1.rb
+++ AA study/review/rspec_exercise_1/lib/part_1.rb
@@ -5,3 +5,23 @@ end
 def average_array(arr)
 	arr.sum()/(arr.length.to_f)
 end
+
+def repeat(str, num)
+	result = ''
+	num.times { result << str }
+	result
+end
+
+def yell(str)
+	str.chars.map {|ch| ch.upcase()}.join('') + '!'
+end
+
+def alternating_case(str)
+	str.split(' ').map.with_index do |word, i|
+		if(i % 2 == 0)
+			word.upcase()
+		else
+			word.downcase()
+		end
+	end.join(' ')
+end

63b94b54d343ede8cab98caa5876de35c1240b13
Richard Ma <rich.maaa@gmail.com>
2020-10-07 01:16:43 -0700

    finish rspec part2

--- AA study/review/rspec_exercise_1/lib/part_2.rb
+++ AA study/review/rspec_exercise_1/lib/part_2.rb
@@ -7,9 +7,28 @@ def hipsterfy(string)
 	string.reverse()
 end
 
+# def vowel_counts(str)
+# 	hash = Hash.new(0)
+# 	vowel_arr = str.downcase.scan(/[aeiou]/)
+# 	vowel_arr.each { |ch| hash[ch] += 1 }
+# 	hash
+# end
+
 def vowel_counts(str)
 	hash = Hash.new(0)
-	vowel_arr = str.downcase.scan(/[aeiou]/)
-	vowel_arr.each { |ch| hash[ch] += 1 }
+	str.downcase.chars.each do |ch|
+		hash[ch] += 1 if 'aeiou'.include?(ch)
+	end
 	hash
+end
+
+def caesar_cipher(str, n)
+	alphabet = ('a'..'z').to_a
+	str.chars.map do |ch|
+		if alphabet.index(ch)
+			alphabet[(alphabet.index(ch) + n) % alphabet.length]
+		else
+			ch
+		end
+	end.join('')
 end

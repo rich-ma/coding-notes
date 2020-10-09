3986b7041137b12bb9d0e3e1509d2eaa6b4c6ebf
Richard Ma <rich.maaa@gmail.com>
2020-10-08 19:41:13 -0700

    finish censor, add support method

--- AA study/review/rspec_exercise_2/lib/part_1.rb
+++ AA study/review/rspec_exercise_2/lib/part_1.rb
@@ -12,3 +12,24 @@ def merge(hash_1, hash_2)
 	hash_2.each { |key, val| merged_hash[key] = val }
 	merged_hash
 end
+
+def censor(str, arr)
+	str.split(' ').map do |word|
+		if arr.include?(word.downcase)
+			censorfy(word)
+		else
+			word
+		end
+	end.join(' ')
+end
+
+def censorfy(word)
+	vowels = 'aeiou'
+	word.chars.map do |ch|
+		if vowels.include?(ch.downcase)
+			'*'
+		else
+			ch
+		end
+	end.join('')
+end

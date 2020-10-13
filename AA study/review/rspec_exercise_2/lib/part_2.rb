12528a235c3286a966e5c1836d84006181ef670e
Richard Ma <rich.maaa@gmail.com>
2020-10-12 19:37:42 -0700

    finish rspec 2 part 2

--- AA study/review/rspec_exercise_2/lib/part_2.rb
+++ AA study/review/rspec_exercise_2/lib/part_2.rb
@@ -1,7 +1,30 @@
 def palindrome?(str)
 	(str.length/2).times do |i|
 		return false if str[i] != str[-i - 1]
-
 	end
-true
+	true
+end
+
+def substrings(str)
+	result = []
+	str.chars.each_with_index do |ch, i|
+		j = i
+		while j < str.length
+			result << str[i..j]
+			j += 1
+		end
+	end
+
+	result
+end
+
+def palindrome_substrings(str)
+	results = []
+	substrings = substrings(str)
+	substrings.each do |sub|
+		if sub.length > 1 && palindrome?(sub)
+			results << sub
+		end
+	end
+	results
 end

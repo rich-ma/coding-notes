9b4d202f74cffdcad7af2654a3a7af3f501b2573
Richard Ma <rich.maaa@gmail.com>
2021-01-26 18:22:09 -0800

    finish first three block proj

--- AA study/blocks_project/lib/part_1.rb
+++ AA study/blocks_project/lib/part_1.rb
@@ -1,3 +1,11 @@
 def select_even_nums(arr)
  arr.select(&:even?)
 end
+
+def reject_puppies(arr)
+	arr.reject{|dog| dog['age'] <= 2}
+end
+
+def count_positive_subarrays(arr)
+	arr.count{|el| el.inject(0){|sum,i| sum + i} > 0}
+end

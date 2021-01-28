dc65f76882e9d41596a7ca874d3a6f90dac6828b
Richard Ma <rich.maaa@gmail.com>
2021-01-27 18:01:09 -0800

    finish block proj

--- AA study/blocks_project/lib/part_1.rb
+++ AA study/blocks_project/lib/part_1.rb
@@ -9,3 +9,12 @@ end
 def count_positive_subarrays(arr)
 	arr.count{|el| el.inject(0){|sum,i| sum + i} > 0}
 end
+
+def aba_translate(str)
+	vowels = 'aeiou'
+	str.chars.map {|el| vowels.include?(el) ? el + 'b' + el : el }.join('')
+end
+
+def aba_array(arr)
+	arr.map{ |el| aba_translate(el)}
+end
--- AA study/blocks_project/spec/00_part_1_spec.rb
+++ AA study/blocks_project/spec/00_part_1_spec.rb
@@ -55,45 +55,45 @@ describe "PART 1" do
   #   end
   # end
 
-  describe "count_positive_subarrays" do
-    it "should accept a 2D array as an arg" do
-      array = [
-        [-2, 5],
-        [1, -9, 1],
-        [4, 7]
-      ]
+  # describe "count_positive_subarrays" do
+  #   it "should accept a 2D array as an arg" do
+  #     array = [
+  #       [-2, 5],
+  #       [1, -9, 1],
+  #       [4, 7]
+  #     ]
 
-      expect { count_positive_subarrays(array) }.to_not raise_error
-    end
+  #     expect { count_positive_subarrays(array) }.to_not raise_error
+  #   end
 
-    it "should return the number of subarrays whose elements sum to a positive quantity" do
-      array_1 = [
-        [-2, 5],
-        [1, -9, 1],
-        [4, 7]
-      ]
-      expect(count_positive_subarrays(array_1)).to eq(2)
+  #   it "should return the number of subarrays whose elements sum to a positive quantity" do
+  #     array_1 = [
+  #       [-2, 5],
+  #       [1, -9, 1],
+  #       [4, 7]
+  #     ]
+  #     expect(count_positive_subarrays(array_1)).to eq(2)
 
-      array_2 = [
-        [4],
-        [-20, 1, 3],
-        [4, -5],
-        [-1000, 1000, -1, 1]
-      ]
-      expect(count_positive_subarrays(array_2)).to eq(1)
-    end
+  #     array_2 = [
+  #       [4],
+  #       [-20, 1, 3],
+  #       [4, -5],
+  #       [-1000, 1000, -1, 1]
+  #     ]
+  #     expect(count_positive_subarrays(array_2)).to eq(1)
+  #   end
 
-    it "should use Array#count" do
-      array = [
-        [-2, 5],
-        [1, -9, 1],
-        [4, 7]
-      ]
+  #   it "should use Array#count" do
+  #     array = [
+  #       [-2, 5],
+  #       [1, -9, 1],
+  #       [4, 7]
+  #     ]
 
-      expect(array).to receive(:count).at_least(:once)
-      count_positive_subarrays(array)
-    end
-  end
+  #     expect(array).to receive(:count).at_least(:once)
+  #     count_positive_subarrays(array)
+  #   end
+  # end
 
   # describe "aba_translate" do
   #   it "should accept a word string as an arg" do
@@ -109,25 +109,25 @@ describe "PART 1" do
   #   end
   # end
 
-  # describe "aba_array" do
-  #   it "should accept an array of words as an arg" do
-  #     expect { aba_array(["cat", "dog", "butterfly"]) }.to_not raise_error
-  #   end
+  describe "aba_array" do
+    it "should accept an array of words as an arg" do
+      expect { aba_array(["cat", "dog", "butterfly"]) }.to_not raise_error
+    end
 
-  #   it "should return a new array where every word has been 'aba translated'" do
-  #     expect(aba_array(["cat", "dog", "butterfly"])).to eq(["cabat", "dobog", "bubutteberfly"])
-  #     expect(aba_array(["fly", "kite"])).to eq(["fly", "kibitebe"])
-  #   end
+    it "should return a new array where every word has been 'aba translated'" do
+      expect(aba_array(["cat", "dog", "butterfly"])).to eq(["cabat", "dobog", "bubutteberfly"])
+      expect(aba_array(["fly", "kite"])).to eq(["fly", "kibitebe"])
+    end
 
-  #   it "should use Array#map" do
-  #     array = ["cat", "dog", "butterfly"]
-  #     expect(array).to receive(:map).at_least(:once)
-  #     aba_array(array)
-  #   end
+    it "should use Array#map" do
+      array = ["cat", "dog", "butterfly"]
+      expect(array).to receive(:map).at_least(:once)
+      aba_array(array)
+    end
 
-  #   it "should use your previous aba_translate method" do
-  #     expect(self).to receive(:aba_translate).at_least(:once)
-  #     aba_array(["cat", "dog", "butterfly"])
-  #   end
-  # end
+    it "should use your previous aba_translate method" do
+      expect(self).to receive(:aba_translate).at_least(:once)
+      aba_array(["cat", "dog", "butterfly"])
+    end
+  end
 end

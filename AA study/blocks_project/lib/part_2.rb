e1a73018268d9f980368c96c8fccb7dc252d2ff2
Richard Ma <rich.maaa@gmail.com>
2021-01-28 17:36:41 -0800

    add alt solution to block2

--- AA study/blocks_project/lib/part_2.rb
+++ AA study/blocks_project/lib/part_2.rb
@@ -1,14 +1,27 @@
+# def all_words_capitalized?(arr)
+# 	arr.all? do |word|
+# 		word[0] == word[0].upcase && word[1..-1] == word[1..-1].downcase
+# 	end
+# end
+
 def all_words_capitalized?(arr)
 	arr.all? do |word|
-		word[0] == word[0].upcase && word[1..-1] == word[1..-1].downcase
+		word == word.capitalize
 	end
 end
 
+# def no_valid_url?(arr)
+#  valid_end = ['com', 'net', 'io','org']
+#  arr.none? do |url|
+# 	top_level = url.split('.')[1]
+# 	valid_end.include?(top_level)
+#  end
+# end
+
 def no_valid_url?(arr)
- valid_end = ['com', 'net', 'io','org']
+ valid_end = ['.com', '.net', '.io','.org']
  arr.none? do |url|
-	top_level = url.split('.')[1]
-	valid_end.include?(top_level)
+	valid_end.any? {|ending| url.end_with?(ending) }
  end
 end
 

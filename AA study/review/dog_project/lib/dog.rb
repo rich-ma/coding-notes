d3d18e7476e4518f993c6f004c5fe27304b447e8
Richard Ma <rich.maaa@gmail.com>
2021-02-04 14:59:57 -0800

    finish dog class proj

--- AA study/review/dog_project/lib/dog.rb
+++ AA study/review/dog_project/lib/dog.rb
@@ -1,3 +1,49 @@
 class Dog
+	def initialize(name, breed, age, bark, favorite_foods)
+		@name = name
+		@breed = breed
+		@age = age
+		@bark = bark
+		@favorite_foods = favorite_foods
+	end
+
+	#getters
+	def name
+		@name
+	end
+
+	def breed
+		@breed
+	end
+
+	def age
+		@age
+	end
+
+	def bark
+		if self.age > 3
+			@bark.upcase
+		else
+			@bark.downcase
+		end
+	end
+
+	def favorite_foods
+		@favorite_foods
+	end
+
+	#setters
+	def age=(num)
+		@age = num
+	end
+
+	#methods
+	def favorite_food?(str)
+		@favorite_foods.include?(str.capitalize)
+	end
+
+
+
+
 
 end

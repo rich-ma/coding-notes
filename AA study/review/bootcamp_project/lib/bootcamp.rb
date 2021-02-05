67258bd6e8c97601a03e3b94bce37d04567ed0f7
Richard Ma <rich.maaa@gmail.com>
2021-02-04 17:46:49 -0800

    finish first half bc proj

--- AA study/review/bootcamp_project/lib/bootcamp.rb
+++ AA study/review/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,51 @@
 class Bootcamp
-  
+	def initialize(name, slogan, student_capacity)
+		@name = name
+		@slogan = slogan
+		@student_capacity = student_capacity
+		@students = []
+		@teachers = []
+		@grades = Hash.new{|h,k| h[k] = []}
+
+	end
+
+	#getters
+	def name
+		@name
+	end
+
+	def slogan
+		@slogan
+	end
+
+	def teachers
+		@teachers
+	end
+
+	def students
+		@students
+	end
+
+
+
+	#methods
+	def hire(teacher)
+		@teachers << teacher
+	end
+
+	def enroll(student)
+		if students.length < @student_capacity
+			@students << student
+			return true
+		else
+			return false
+		end
+	end
+
+	def enrolled?(student)
+		@students.include?(student) ? true : false
+	end
+
+
+
 end

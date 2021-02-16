dc2b3cf286ef1cf44290c560497606c798b1f6fd
Richard Ma <rich.maaa@gmail.com>
2021-02-15 21:01:22 -0800

    finish part 1 hotel proj

--- AA study/review/hotel_project/lib/room.rb
+++ AA study/review/hotel_project/lib/room.rb
@@ -1,3 +1,35 @@
 class Room
+	def initialize(capacity)
+		@capacity = capacity
+		@occupants = []
+
+	end
+
+	#getters
+	def capacity
+		@capacity
+	end
+
+	def occupants
+		@occupants
+	end
+
+	#methods
+	def full?
+		@occupants.length >= @capacity
+	end
+
+	def available_space
+		@capacity - @occupants.length
+	end
+
+	def add_occupant(person)
+		if self.full?
+			false
+		else
+			@occupants << person
+			true
+		end
+	end
 
 end

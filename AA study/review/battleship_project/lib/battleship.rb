68e0970067db101d1a89e8b7e0c5d73ac6c6e769
Richard Ma <rich.maaa@gmail.com>
2021-02-19 19:39:09 -0800

    finish battleship

--- AA study/review/battleship_project/lib/battleship.rb
+++ AA study/review/battleship_project/lib/battleship.rb
@@ -2,5 +2,50 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+	attr_reader :board, :player
+
+	def initialize(n)
+		@player = Player.new
+		@board = Board.new(n)
+		@remaining_misses = @board.size/2
+	end
+
+	def start_game
+		board.place_random_ships
+		p board.num_ships
+		board.print
+	end
+
+	def lose?
+		if @remaining_misses <= 0
+			p 'you lose'
+			true
+		else
+			false
+		end
+	end
+
+	def win?
+		if board.num_ships <= 0
+			p 'you win'
+			true
+		else
+			false
+		end
+	end
+	
+	def game_over?
+		return true if win? || lose?
+
+		false
+	end
+
+	def turn
+		move = player.get_move
+		hit = board.attack(move)
+		@remaining_misses -= 1 unless hit
+		board.print
+		p @remaining_misses
+	end
 
 end

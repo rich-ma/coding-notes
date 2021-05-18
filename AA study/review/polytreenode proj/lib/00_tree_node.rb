c022e79227366b6282b798f6b5451315a82b9b7e
Richard Ma <rich.maaa@gmail.com>
2021-05-17 18:56:36 -0700

    test child node

--- AA study/review/polytreenode proj/lib/00_tree_node.rb
+++ AA study/review/polytreenode proj/lib/00_tree_node.rb
@@ -19,11 +19,29 @@ class PolyTreeNode
 	end
 
 	def parent=(node)
+		unless @parent.nil?
+			remove_child(node, self)
+		end
 		@parent = node
+		if node
+			node.add_child(self)
+		end
+	end
+
+	def remove_child(node, child)
+		children = node.children
+		i = children.index(child)
+		node.children = children.slice(0,i).concat(children.slice(i+1..-1))
 	end
 
 	def children=(node)
-		
+		@children = node
+	end
+
+	def add_child(node)
+		unless @children.include?(node)
+			@children << node
+		end
 	end
 end
 

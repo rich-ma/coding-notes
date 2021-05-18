f277b287e2d8baa61d209e2da55fe51f722eaf6a
Richard Ma <rich.maaa@gmail.com>
2021-05-17 17:39:09 -0700

    add getters

--- AA study/review/polytreenode proj/lib/00_tree_node.rb
+++ AA study/review/polytreenode proj/lib/00_tree_node.rb
@@ -1,3 +1,29 @@
 class PolyTreeNode
 
+	def initialize(node)
+		@value = node
+		@parent = nil
+		@children = []
+	end
+
+	def value
+		@value
+	end
+
+	def parent
+		@parent
+	end
+
+	def children
+		@children
+	end
+
+	def parent=(node)
+		@parent = node
+	end
+
+	def children=(node)
+		
+	end
 end
+
